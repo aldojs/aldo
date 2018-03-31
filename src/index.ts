770c673b7e17f2369e4d05ba02a5a6491644cf5c
absolux <a.mouhassine@gmail.com>
2018-03-31 15:07:14 +0100

    Export the router to a separate repo

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
 
-export { default as Route } from './route'
-export { default as Router } from './router'
 export { default as Application } from './application'
--- test/router.ts
+++
@@ -1,8 +0,0 @@
-
-import 'mocha'
-import Router from '../src/router'
-import * as assert from 'assert'
-
-describe('Test Router', () => {
-  describe('router.use', () => {})
-})
