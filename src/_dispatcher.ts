3bb85227404499b40a030bb3f67a29c3e36a9bdc
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2018-06-05 18:26:58 +0000

    Update src/index.ts

---
+++ src/_dispatcher.ts
@@ -0,0 +1,61 @@
+
+import is from '@sindresorhus/is'
+
+export interface IMiddleware<T> {
+  (input: T, next: () => any): any
+}
+
+/**
+ * Middleware dispatcher class
+ */
+export class Dispatcher<T> {
+  /**
+   * The middleware stack
+   * 
+   * @private
+   */
+  private _middlewares: IMiddleware<T>[]
+
+  /**
+   * Initialize a new dispatcher instance
+   * 
+   * @param stack the middleware stack
+   */
+  public constructor (stack: IMiddleware<T>[] = []) {
+    this._middlewares = stack
+  }
+
+  /**
+   * Use a middleware
+   * 
+   * @param fn The middleware function
+   * @public
+   */
+  public use (fn: IMiddleware<T>): this {
+    this._middlewares.push(fn)
+    return this
+  }
+
+  /**
+   * Dispatch the given input to the middlewares
+   * 
+   * @param input
+   * @public
+   */
+  public dispatch (input: T): any {
+    return this._invoke(0, input)
+  }
+
+  /**
+   * Invoke the current middleware
+   * 
+   * @param idx The current middleware index
+   * @param arg The input to dispatch
+   * @private
+   */
+  private _invoke (idx: number, arg: T): any {
+    let fn = this._middlewares[idx]
+
+    if (fn) return fn(arg, () => this._invoke(idx + 1, arg))
+  }
+}
